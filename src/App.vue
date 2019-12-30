adf8fc2a9921ab67536ff5a7166f59971e7ba41b
AntiZero <AntiZero91@gmail.com>
2019-12-30 23:37:01 +0300

    Creation of Home form

--- src/App.vue
+++ src/App.vue
@@ -50,7 +50,9 @@
         class="hidden-md-and-up"
       ></v-app-bar-nav-icon>
 
-      <v-toolbar-title>Ad application</v-toolbar-title>
+      <v-toolbar-title>
+        <router-link to="/" tag="span" class="pointer">Ad application</router-link>
+      </v-toolbar-title>
 
       <v-spacer></v-spacer>
 
@@ -110,3 +112,9 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+  .pointer {
+    cursor: pointer;
+  }
+</style>
