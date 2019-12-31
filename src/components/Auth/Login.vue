56fd23665790637a6cdca2423231e34ab32ebe78
AntiZero <AntiZero91@gmail.com>
2020-01-01 01:56:50 +0300

    Login in Firebase

--- src/components/Auth/Login.vue
+++ src/components/Auth/Login.vue
@@ -52,7 +52,8 @@
             <v-btn
               color="primary"
               @click="onSubmit"
-              :disabled="!valid"
+              :loading="loading"
+              :disabled="!valid || loading"
               >Login</v-btn>
           </v-card-actions>
         </v-card>
@@ -78,6 +79,11 @@ export default {
       ]
     }
   },
+  computed: {
+    loading () {
+      return this.$store.getters.loading
+    }
+  },
   methods: {
     onSubmit () {
       if (this.$refs.form.validate()) {
@@ -85,7 +91,12 @@ export default {
           email: this.email,
           password: this.password
         }
-        console.log(user)
+
+        this.$store.dispatch('loginUser', user)
+          .then(() => {
+            this.$router.push('/')
+          })
+          .catch(err => console.log(err))
       }
     }
   }
